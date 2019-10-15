1e239bdfb72e36382926bf856da4bc6d06104342
piotrrus <irache@wp.pl>
2019-10-15 22:51:51 +0200

    Update Payments.form.ts

--- Payments.form.ts
+++ Payments.form.ts
@@ -1,38 +1,27 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Base } from './Base.form';
-
 import { textValidator } from '../validators/custom-text.validator';
-import { Subscription } from 'rxjs';
-// import { PatternsLibraryService } from '../services/pattern-library.service';
-/*
-import {distinctUntilChanged} from 'rxjs/operators';
-import {DistinctService} from '../services/utils/distinct.service';
-*/
 
 export class PaymentForm extends Base {
 
     private _form: FormGroup;
-    // private patterns: PatternsLibraryService;
-
+    
     constructor(
         public fb: FormBuilder,
     ) {
         super(fb);
         this.createForm();
     }
-// Validators.pattern(this.patterns['CASH_REGEXP'])
-// Validators.pattern('/^[0-9]*$/u')
+   
     createForm(): FormGroup {
         this.form = this.fb.group({
             name: ['', [Validators.required, textValidator, Validators.maxLength(30)]],
             surname: ['', [Validators.required, textValidator, Validators.maxLength(30)]],
             account: ['', [Validators.required, , Validators.maxLength(26)]],
-
             amount: ['', [Validators.required, Validators.maxLength(26)]],
             payment_date: ['', [Validators.required, Validators.maxLength(12)]],
             payment_purpose: ['', [Validators.required, Validators.maxLength(255)]]
         });
-
         return this.form;
     }
 
