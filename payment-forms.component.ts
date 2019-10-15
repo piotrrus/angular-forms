9c4f069e1b46671f57c0d3bafe607d0d9811004c
piotrrus <irache@wp.pl>
2019-10-15 22:47:31 +0200

    Update payment-forms.component.ts

--- payment-forms.component.ts
+++ payment-forms.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { BaseComponent } from '../base/base.component';
 import { ErrorMsgService } from '../../services/error-msg.service';
 import { PaymentForm } from '../../forms/Payments.form';
-
-// import {PatternsLibraryService} from '../../services/pattern-library.service';
-
 
 @Component({
   selector: 'app-payment-forms',
@@ -16,8 +13,7 @@ import { PaymentForm } from '../../forms/Payments.form';
 export class PaymentFormsComponent extends BaseComponent  implements OnInit {
 
   public paymentForm: PaymentForm;
-  //validationService: ErrorMsgService;
-
+  
   constructor(
     private fb: FormBuilder,
     private vs: ErrorMsgService,
@@ -28,41 +24,4 @@ export class PaymentFormsComponent extends BaseComponent  implements OnInit {
 
   ngOnInit() {
   }
-
-  /*private createForms(): void {
-    this.paymentForm = this.fb.group({
-      payments: this.fb.array([])
-    });
-  }*/
-
-  /*get payments(): FormArray {
-    return this.paymentForm.get('payments') as FormArray;
-  }*/
-
-  /*public addPayment(): void {
-    let singlePaymentForm = this.fb.group({
-      pepData: this._pep.createForm(),
-        //fatherName: ['', [Validators.maxLength(15), textValidator]],
-        //motherName: ['', [Validators.maxLength(15), textValidator]]
-    });
-
-    // do formy !!
-    this.handleSinglePaymentForm(singlePrincipalForm); //
-
-    this.payments.push(singlePaymentForm);
-}*/
-
-  // private setValues(data): void {
-  //   const formCtrls = this.payments.controls[this.choosenFormId];
-  //   const newData = data.attributes;
-  //   const contactData = newData.contactData;
-  //   const personDocument = newData.personDocument;
-
-  //   formCtrls.get('name').setValue(newData.name);
-  // }
-
-  // public getErrorMessage(validatorName) {
-  //   return this.validationService.getValidatorErrorMessage(validatorName);
-  // }
-
 }
